4e62560257ffa3f45e75dcd3789dfb569b6b60a4
Mateusz Krawczyk <krawat10@gmail.com>
2019-11-11 23:00:41 +0100

    first commit

---
+++ config/settings.old.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'prestashopuser');
+define('_DB_PASSWD_', 'Informatyka2019');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheFs');
+define('_PS_CACHE_ENABLED_', '1');
+define('_COOKIE_KEY_', 'W1peHPhfWbxHsNXbZY9K6e9Yf1ahnRmUotwLhxm8yuK7bWR6DD2EFsSU');
+define('_COOKIE_IV_', '4MDqQac6');
+define('_PS_CREATION_DATE_', '2019-10-16');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.5');
+define('_RIJNDAEL_KEY_', 'efjp4AnusUcdOZCbZHI78Mq6YNghwx4v');
+define('_RIJNDAEL_IV_', 'PTkqwyMWJVzi2jLcBeqffw==');
---
+++ modules/blockcustomerprivacy/translations/pl.php
@@ -0,0 +1,17 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+
+$_MODULE['<{blockcustomerprivacy}prestashop>blockcustomerprivacy_e0de5a06213f21c55ca3283c009e0907'] = 'Blok ochrona danych osobowych';
+$_MODULE['<{blockcustomerprivacy}prestashop>blockcustomerprivacy_f192f208f0bc97af4c5213eee3e78793'] = 'Dodaje blok wyświetlający informacje o przetwarzaniu danych osobowych.';
+$_MODULE['<{blockcustomerprivacy}prestashop>blockcustomerprivacy_20015706a8cbd457cbb6ea3e7d5dc9b3'] = 'Konfiguracja zaktualizowana';
+$_MODULE['<{blockcustomerprivacy}prestashop>blockcustomerprivacy_fb32badede7c8613fddb8502d847c18b'] = 'Nie można założyć konta z powodu braku zgody na przetwarzanie danych osobowych.';
+$_MODULE['<{blockcustomerprivacy}prestashop>blockcustomerprivacy_f4f70727dc34561dfde1a3c529b6205c'] = 'Ustawienia';
+$_MODULE['<{blockcustomerprivacy}prestashop>blockcustomerprivacy_03e1a999dcdb904300ee1b1e767c83c9'] = 'Wiadomość, która zostanie wyświetlona w formularzu zakładania nowego konta klienta';
+$_MODULE['<{blockcustomerprivacy}prestashop>blockcustomerprivacy_b51d73fb490ad1245fa9b87042bbbbb7'] = 'Wskazówka: Pamiętaj, jeżeli tekst jest zbyt długi aby go wpisać bezpośrednio do formularza, można podać link do jednej ze stron stworzonej za pomocą strony „CMS” w menu „Preferencje”.';
+$_MODULE['<{blockcustomerprivacy}prestashop>blockcustomerprivacy_c9cc8cce247e49bae79f15173ce97354'] = 'Zapisz';
+$_MODULE['<{blockcustomerprivacy}prestashop>blockcustomerprivacy_fb0440f9ca32a8b49eded51b09e70821'] = 'Ochrona danych osobowych';
+
+
+return $_MODULE;
